26e955fb57b9d2fe3b00d4920f5ce684dbbb88d9
jihanstkm <jihanstk@gmail.com>
2023-05-18 10:55:41 +0600

    server is initialies

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5021;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("toys Infinity is running");
+});
+
+app.listen(port, () => {
+  console.log(`toys is running on ${port}`);
+});
